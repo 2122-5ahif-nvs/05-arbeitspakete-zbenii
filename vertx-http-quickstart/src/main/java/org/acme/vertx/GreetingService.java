a4454785c52dea23be29fec389bdf2e1cc3a3f87
zbenii <besic.beni@gmx.at>
2021-11-26 12:59:15 +0100

    added GreetingService

---
+++ vertx-http-quickstart/src/main/java/org/acme/vertx/GreetingService.java
@@ -0,0 +1,15 @@
+package org.acme.vertx;
+
+import io.quarkus.vertx.ConsumeEvent;
+
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class GreetingService {
+
+    @ConsumeEvent("greeting")
+    public String greeting(String name) {
+        return "Hello " + name;
+    }
+
+}}
