a87ff5d46cadc0f03212476cdcd2e9754eb5b8ec
zbenii <besic.beni@gmx.at>
2021-11-26 12:48:36 +0100

    added message as constructor for consume

--- vertx-quickstart/src/main/java/org/acme/vertx/GreetingService.java
+++ vertx-quickstart/src/main/java/org/acme/vertx/GreetingService.java
@@ -1,19 +1,42 @@
 package org.acme.vertx;
 
 import io.quarkus.vertx.ConsumeEvent;
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.eventbus.Message;
+import org.eclipse.microprofile.context.ManagedExecutor;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 
 @ApplicationScoped
 public class GreetingService {
+    /*
+    @Inject
+    ManagedExecutor executor;
+     */
 
+    @ConsumeEvent("greeting")
+    public void consume(Message<String> msg) {
+        System.out.println(msg.address());
+        System.out.println(msg.body());
+    }
+
+    /*
     @ConsumeEvent("greeting")
     public String consume(String name) {
         return name.toUpperCase();
+    }
+     */
+
+    /*
+    @ConsumeEvent("greeting")
+    public Uni<String> consume2(String name) {
+        return Uni.createFrom().item(() -> name.toUpperCase()).emitOn(executor);
     }
 
     @ConsumeEvent(value = "blocking-consumer", blocking = true)
     void consumeBlocking(String message) {
         // Something blocking
     }
+    */
 }
